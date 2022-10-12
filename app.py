9cbcae339cd93088f97454c6f55278b162667748
Leo <drakenwarwolf@gmail.com>
2022-10-12 17:14:32 -0400

    Adding all files v1.0

--- app.py
+++ app.py
@@ -6,19 +6,20 @@ import datetime
 app = Flask(__name__)
 global studentOrganisationDetails
 # Assign default 5 values to studentOrganisationDetails for Application  3.
-
+studentOrganisationDetails = {"John Doe": "Women Who Code", "Jane Doe": "Women Who Code", "Jackson Doe": "Charlotte Hack",
+ "Lorna Doe": "Charlotte Hack", "Liam Doe": "Charlotte Hack"}
 
 @app.get('/')
 def index():
     # Complete this function to get current date and time assign this value to currentDate, display this data on index.html
-
+    currentDate = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return render_template('index.html', currentDate=currentDate)
 
 
 @app.get('/calculate')
 def displayNumberPage():
     # Complete this function to display form.html page
-    pass
+    return render_template('form.html')
 
 
 @app.route('/calculate', methods=['POST'])
@@ -30,6 +31,15 @@ def checkNumber():
     # Display "Provided input is not an integer!" if value is not a number on result.html page
     global number
     number = request.form['number']
+    if number == "":
+        return render_template('result.html', message="No number provided")
+    elif number.isdigit():
+        if int(number) % 2 == 0:
+            return render_template('result.html', message="Number " + number + " is even")
+        else:
+            return render_template('result.html', message="Number " + number + " is odd")
+    else:
+        return render_template('result.html', message="Provided input is not an integer!")
 
     # Write your to code here to check whether number is even or odd and render result.html page
 
@@ -37,14 +47,18 @@ def checkNumber():
 @app.get('/addStudentOrganisation')
 def displayStudentForm():
     # Complete this function to display studentFrom.html page
-    pass
+    return render_template('studentForm.html')
 
 
 @app.route('/addStudentOrganisation', methods=['POST'])
 def displayRegistrationPage():
     # Get student name and organisation from form.
     studentName = request.form['name']
+    studentOrganisation = request.form['organisation']
 
     # Append this value to studentOrganisationDetails
+    studentOrganisationDetails[studentName] = studentOrganisation
+
 
     # Display studentDetails.html with all students and organisations
+    return render_template('studentDetails.html', studentOrganisationDetails=studentOrganisationDetails)
